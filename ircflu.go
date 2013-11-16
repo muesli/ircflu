a26f119b730d39d6b5a4a7fde1abe0041c8b0655
Christian Muehlhaeuser <muesli@gmail.com>
2013-11-16 06:45:03 +0100

    * Use new commands.

--- ircflu.go
+++ ircflu.go
@@ -5,14 +5,19 @@ import (
 
 	"ircflu/app"
 
+	_ "ircflu/commands"
+	_ "ircflu/commands/alias"
+	_ "ircflu/commands/exec"
+	_ "ircflu/commands/join"
+	_ "ircflu/commands/send"
+
 	"ircflu/msgsystem"
 	_ "ircflu/msgsystem/catserver"
 	_ "ircflu/msgsystem/irc"
 	_ "ircflu/msgsystem/web"
-
-	_ "ircflu/hooks"
-	_ "ircflu/hooks/github"
-	_ "ircflu/hooks/gitlab"
+	_ "ircflu/msgsystem/web/hooks"
+	_ "ircflu/msgsystem/web/hooks/github"
+	_ "ircflu/msgsystem/web/hooks/gitlab"
 )
 
 func main() {
