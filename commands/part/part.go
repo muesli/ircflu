ab9b26745ffad59a5d87fc0fd716a11e2ed05c9b
Christian Muehlhaeuser <muesli@gmail.com>
2013-11-16 07:42:45 +0100

    * Added PartCommand.

---
+++ commands/part/part.go
@@ -0,0 +1,67 @@
+package partCmd
+
+import (
+	"fmt"
+	_ "log"
+	"strings"
+	"ircflu/commands"
+	"ircflu/msgsystem"
+	"ircflu/msgsystem/irc"
+)
+
+type PartCommand struct {
+	messagesIn chan msgsystem.Message
+	messagesOut chan msgsystem.Message
+}
+
+func (h *PartCommand) Name() string {
+	return "part"
+}
+
+func (h *PartCommand) MessageInChan() chan msgsystem.Message {
+	return h.messagesIn
+}
+
+func (h *PartCommand) SetMessageInChan(channel chan msgsystem.Message) {
+	h.messagesIn = channel
+}
+
+func (h *PartCommand) MessageOutChan() chan msgsystem.Message {
+	return h.messagesOut
+}
+
+func (h *PartCommand) SetMessageOutChan(channel chan msgsystem.Message) {
+	h.messagesOut = channel
+}
+
+func (h *PartCommand) Parse(msg msgsystem.Message) bool {
+	channel := msg.To
+	m := strings.Split(msg.Msg, " ")
+	cmd := m[0]
+	params := strings.Join(m[1:], " ")
+
+	switch cmd {
+		case "!part":
+			if len(params) > 0 {
+				fmt.Println("Parting:", params)
+
+				ircclient := (*msgsystem.SubSystem("irc")).(*irc.IrcSubSystem)
+				if ircclient != nil {
+					ircclient.Part(params)
+				}
+			} else {
+				r := msgsystem.Message{
+					To: channel,
+					Msg: "Usage: !part #chan",
+				}
+				h.messagesOut <- r
+			}
+			return true
+	}
+	return false
+}
+
+func init() {
+	partCmd := PartCommand{}
+	commands.RegisterCommand(&partCmd)
+}
